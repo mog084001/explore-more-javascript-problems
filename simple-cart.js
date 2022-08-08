dec3308739efce8acd2b46eef7f26a809a63de32
mog084001 <mong.86duet@gmail.com>
2022-08-08 13:41:15 +0600

    new added

---
+++ simple-cart.js
@@ -0,0 +1,21 @@
+const shoppingCart = [
+    { name: 'shoe', price: 1200 },
+    { name: 'shirt', price: 2200 },
+    { name: 'pqnt', price: 3700 },
+    { name: 'belt', price: 1200 },
+    { name: 'gengi', price: 1200 },
+    { name: 'golganzi', price: 1000 },
+];
+
+function totalCost(products) {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        sum = sum + product.price;
+        // console.log(product);
+    }
+    return sum;
+}
+
+const expense = totalCost(shoppingCart);
+console.log('Total expense today:', expense);
