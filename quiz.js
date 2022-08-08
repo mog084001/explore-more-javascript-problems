a90f5d6a54090c0c49a5ecd227981a8526ffb39d
mog084001 <mong.86duet@gmail.com>
2022-08-08 16:41:48 +0600

    quiz added

---
+++ quiz.js
@@ -0,0 +1,27 @@
+// const nayoks = ["Bangla Bhai", "English Bhai", "Korean Bhai", "Turkish Bhai"];
+// for (const nayok on nayoks) {
+
+// }
+// console.log(nayoks);
+// const nayoks = ["Bangla Bhai", "English Bhai", "Korean Bhai", "Turkish Bhai"];
+// for (let i = 0; i <= 5; i++) {
+//     console.log(nayoks);
+// }
+
+
+// const nayoks = ["Bangla Bhai", "English Bhai", "Korean Bhai", "Turkish Bhai"];
+// for (const nayok of nayoks) {
+//     console.log(nayoks);
+// }
+
+
+// const nayoks = ["Bangla Bhai", "English Bhai", "Korean Bhai", "Turkish Bhai"];
+// for (const nayok inside nayoks) {
+//     console.log(nayoks);
+// }
+// console.log(Math.min(9, 0));
+let sum = 0;
+for (let i = 0; i <= 3; i++) {
+    sum = sum + i;
+    console.log(sum);
+}
